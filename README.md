fd6e7c47ae7ba7c0e61da1cc0e4040e30fc07d5c
Travis Garza <garza.travis13@gmail.com>
2014-12-01 12:43:31 -0500

    Function HTTP resquests
    express.js has the support to handle HTTP requests.
    
    express.test.js can execute requests of post, put, get, and delete and
    check whether they were successful.
    
    README is updated with a more clear goal, progress of the goal, and
    dependencies used in express.js and express.test.js

--- README.md
+++ README.md
@@ -2,9 +2,16 @@ OADA-Streaming-Test
 ===================
 
 A program that will test to see whether a server and database is OADA compliant by:
-	-Generating JSON objects with varying sizes and traits
-	-Execute HTTP requests such as GET, POST, PUT, DELETE with the JSON objects already generated
-	-Continually execute these HTTP requests over a period of time
-	-Check to see if a database can handle all of the above with produce output in OADA standards found here: http://openag.io/
+	-Generating JSON objects with varying sizes and values (incomplete)
+	-Execute HTTP requests such as GET, POST, PUT, DELETE with express, node.js, and mongo db (complete)
+	-Continually execute these HTTP requests over a period of time (incomplete)
+	-Check to see if a database can handle all of the above with produce output in OADA standards found here: http://openag.io/ (incomplete)
 
+Dependencies used are: 
+	-express
+	-mongoskin
+	-bodyParser
+	-superagent
+	-expect.js
+	-mocha
 	
--- express.js
+++ express.js
@@ -1,48 +1,61 @@
+/* *****************************
+ * Dependencies for the Rest-API
+ * *****************************
+ */
 var express = require('express')
   , mongoskin = require('mongoskin')
-    bodyParser = require('body-parser')
+  , bodyParser = require('body-parser')
 
 var app = express()
-app.use(bodyParser)
+app.use(bodyParser())
 
-var db = mongoskin.db('localhost:27017/test', {safe:true});
+/* **********************
+ * Declaring the database
+ * **********************
+ */
+var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true}) 
+//change the first argument to go to a different database
+//make sure the database in use is the same as the path extension
 
 app.param('collectionName', function(req, res, next, collectionName){
   req.collection = db.collection(collectionName)
   return next()
 })
-app.get('/', function(req, res) {
+
+app.get('/', function(req, res, next) {
   res.send('please select a collection, e.g., /collections/messages')
 })
 
-app.get('/collections/:collectionName', function(req, res) {
-  req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+app.get('/collections/:collectionName', function(req, res, next) {
+  req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){ //Right now it only receives 10 items. Increase the limit to display more
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.post('/collections/:collectionName', function(req, res) {
+app.post('/collections/:collectionName', function(req, res, next) {
   req.collection.insert(req.body, {}, function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.get('/collections/:collectionName/:id', function(req, res) {
-  req.collection.findOne({_id: req.collection.id(req.params.id)}, function(e, result){
+app.get('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.findById(req.params.id, function(e, result){
     if (e) return next(e)
     res.send(result)
   })
 })
-app.put('/collections/:collectionName/:id', function(req, res) {
-  req.collection.update({_id: req.collection.id(req.params.id)}, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+
+app.put('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
 })
-app.del('/collections/:collectionName/:id', function(req, res) {
-  req.collection.remove({_id: req.collection.id(req.params.id)}, function(e, result){
+
+app.del('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.removeById(req.params.id, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
